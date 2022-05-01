eee13e36bba738c7497878cf05517fab5505a1bf
Gerardo Zinno <g.zinno@studenti.unipi.it>
2022-05-01 14:40:00 +0200

    Color change

--- src/menusections/MenuNav.tsx
+++ src/menusections/MenuNav.tsx
@@ -12,58 +12,58 @@ export default class MenuNav extends React.Component {
 
                 <Nav.Item>
                     <LinkContainer to="/antipasti">
-                        <Nav.Link>Antipasti</Nav.Link>
+                        <Nav.Link><div className="menu-link">Antipasti</div></Nav.Link>
                     </LinkContainer>
                 </Nav.Item>
 
                 <Nav.Item>
                     <LinkContainer to="/primi">
-                        <Nav.Link>Primi</Nav.Link>
+                        <Nav.Link><div className="menu-link">Primi</div></Nav.Link>
                     </LinkContainer>
                 </Nav.Item>
 
                 <Nav.Item>
                     <LinkContainer to="/secondi">
-                        <Nav.Link>Secondi</Nav.Link>
+                        <Nav.Link><div className="menu-link">Secondi</div></Nav.Link>
                     </LinkContainer>
                 </Nav.Item>
 
                 <Nav.Item>
                     <LinkContainer to="/contorni">
-                        <Nav.Link>Contorni</Nav.Link>
+                        <Nav.Link><div className="menu-link">Contorni</div></Nav.Link>
                     </LinkContainer>
                 </Nav.Item>
 
 
-                <NavDropdown title="Pizze" id="basic-nav-dropdown">
+                <NavDropdown title="Pizze" id="basic-nav-dropdown" >
                     <LinkContainer to="/pizze">
                         <Nav.Link>Tutte</Nav.Link>
                     </LinkContainer>
                     <NavDropdown.Divider></NavDropdown.Divider>
                     <LinkContainer to="/pizzealchiostro">
-                        <Nav.Link>Pizze "Al Chiostro"</Nav.Link>
+                        <Nav.Link><div className="menu-link-dropdown">Pizze "Al Chiostro"</div></Nav.Link>
                     </LinkContainer>
                     <LinkContainer to="/pizzerosse">
-                        <Nav.Link>Pizze Rosse</Nav.Link>
+                        <Nav.Link><div className="menu-link-dropdown">Pizze Rosse</div></Nav.Link>
                     </LinkContainer>
                     <LinkContainer to="/pizzebianche">
-                        <Nav.Link>Pizze Bianche</Nav.Link>
+                        <Nav.Link><div className="menu-link-dropdown">Pizze Bianche</div></Nav.Link>
                     </LinkContainer>
 
                     <LinkContainer to="/pizzecalzoni">
-                        <Nav.Link>Calzoni</Nav.Link>
+                        <Nav.Link><div className="menu-link-dropdown">Calzoni</div></Nav.Link>
                     </LinkContainer>
                 </NavDropdown>
 
                 <Nav.Item>
                     <LinkContainer to="/dolci">
-                        <Nav.Link>Dolci</Nav.Link>
+                        <Nav.Link><div className="menu-link">Dolci</div></Nav.Link>
                     </LinkContainer>
                 </Nav.Item>
 
                 <Nav.Item>
                     <LinkContainer to="/bevande">
-                        <Nav.Link>Bevande</Nav.Link>
+                        <Nav.Link><div className="menu-link">Bevande</div></Nav.Link>
                     </LinkContainer>
                 </Nav.Item>
 
